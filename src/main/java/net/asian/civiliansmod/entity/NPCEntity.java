d1dbee93056f15453d11c7c77ae8408eb97269b8
Suerion <suerion@hotmail.com>
2025-09-02 14:24:24 +0200

    updated gradle to 9.0.0, fixed typo in ClientDialogueSyncPayload.java, fixed the typo in all classes, updated build.gradle
    issue is persistent, not found yet in build 1.21.5

--- src/main/java/net/asian/civiliansmod/entity/NPCEntity.java
+++ src/main/java/net/asian/civiliansmod/entity/NPCEntity.java
@@ -8,7 +8,7 @@ import net.asian.civiliansmod.gui.CustomNPCScreen;
 import net.asian.civiliansmod.gui.DefaultNPCScreen;
 import net.asian.civiliansmod.gui.SlimNPCScreen;
 import java.util.Arrays;
-import net.asian.civiliansmod.networking.payload.npc.dialogue.CilentDialogueSyncPayload;
+import net.asian.civiliansmod.networking.payload.npc.dialogue.ClientDialogueSyncPayload;
 import net.asian.civiliansmod.networking.payload.npc.dialogue.DialogueSyncPayload;
 import net.asian.civiliansmod.networking.payload.npc.dialogue.OpenScreenDialoguesPayload;
 import net.asian.civiliansmod.networking.payload.npc.skin.ClientNpcSkinPayload;
@@ -329,7 +329,7 @@ public class NPCEntity extends PathAwareEntity {
         super.tick();
         if (getWorld().isClient) {
             if (--updateDialoguesTicks == 0) {
-                ClientPlayNetworking.send(new CilentDialogueSyncPayload(this.getUuid()));
+                ClientPlayNetworking.send(new ClientDialogueSyncPayload(this.getUuid()));
             }
         }
     }
