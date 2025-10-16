d9f12486a4aa48371cd60bdf4f19161eb6a1ffbf
Suerion <suerion@hotmail.com>
2025-10-17 00:34:45 +0200

    added more fallbacks and safetynull checks

--- src/main/java/net/asian/civiliansmod/entity/NPCEntity.java
+++ src/main/java/net/asian/civiliansmod/entity/NPCEntity.java
@@ -72,16 +72,16 @@ public class NPCEntity extends PathAwareEntity {
     @Environment(EnvType.CLIENT)
     public boolean dialoguesReceived;
 
-    public SkinManager getSkinManager() {
-        return skinManager;
-    }
     public NameManager getNameManager() {
         return nameManager;
     }
+    public SkinManager getSkinManager() {
+        return skinManager;
+    }
     public ChatManager getChatManager() { return chatManager; }
 
-    SkinManager skinManager = new SkinManager(this);
     NameManager nameManager = new NameManager(this);
+    SkinManager skinManager = new SkinManager(this);
     ChatManager chatManager = new ChatManager(this);
 
     @Override
@@ -600,14 +600,49 @@ public class NPCEntity extends PathAwareEntity {
         public void markDialoguesDirty(UUID avoid) {
             if (!(npc.getWorld() instanceof ServerWorld serverWorld)) return;
 
-            for (ServerPlayerEntity player : serverWorld.getPlayers()) {
-                if (player.getUuid().equals(avoid)) continue;
+            for (ServerPlayerEntity player : serverWorld.getPlayers(p -> !p.getUuid().equals(avoid))) {
                 try {
                     ServerPlayNetworking.send(player, new DialogueSyncPayload(npc.getId(), dialogues));
                 } catch (IOException e) {
-                    e.printStackTrace();
+                    CiviliansMod.LOGGER.error("[CiviliansMod] Failed to sync dialogues to player {}", player.getGameProfile().getName(), e);
                 }
             }
+        }
+
+        //TODO ADD Language entrys....
+        public void updateDialoguesForLanguage(String language, Map<NpcChat.ChatReason, List<String>> newDialogues) {
+            this.dialogues.put(language, new HashMap<>(newDialogues));
+            markDialoguesDirty(null); // Sync to all players
+        }
+
+        public void updateDialogueForLanguageAndReason(String language, NpcChat.ChatReason reason, List<String> messages) {
+            this.dialogues.computeIfAbsent(language, k -> new EnumMap<>(NpcChat.ChatReason.class))
+                    .put(reason, new ArrayList<>(messages));
+            markDialoguesDirty(null);
+        }
+
+        public void addLanguage(String language, Map<NpcChat.ChatReason, List<String>> dialogues) {
+            this.dialogues.put(language, new HashMap<>(dialogues));
+            markDialoguesDirty(null);
+        }
+
+        public void removeLanguage(String language) {
+            this.dialogues.remove(language);
+            markDialoguesDirty(null);
+        }
+
+        public Set<String> getAvailableLanguages() {
+            return dialogues.keySet();
+        }
+
+        public boolean hasLanguage(String language) {
+            return dialogues.containsKey(language) && !dialogues.get(language).isEmpty();
+        }
+
+        public void setLanguageMap(Map<String, Map<NpcChat.ChatReason, List<String>>> newLanguageMap) {
+            this.dialogues.clear();
+            this.dialogues.putAll(newLanguageMap);
+            markDialoguesDirty(null);
         }
 
         static @NotNull BiConsumer<String, Map<NpcChat.ChatReason, List<String>>> getManageCompoundSave(NbtCompound mainCompound) {
@@ -650,6 +685,16 @@ public class NPCEntity extends PathAwareEntity {
             this.npcEntity = npcEntity;
             this.defaultSkin = true;
             this.baseVariant = npcEntity.random.nextInt(88);
+            if (baseVariant <= 43) {
+                this.slim = false;
+            } else {
+                this.slim = true;
+            }
+            if (npcEntity.nameManager != null) {
+                npcEntity.nameManager.setRandomName(this.slim);
+            } else {
+                CiviliansMod.LOGGER.warn("[CiviliansMod] nameManager is null in SkinManager constructor");
+            }
             this.slim = baseVariant > 43;
 
             npcEntity.nameManager.setRandomName(this.slim);
