c094d26d30bb2562c5d6cd0a10a2b60f5fd364d5
Suerion <suerion@hotmail.com>
2025-11-10 19:16:44 +0100

    Preview NPC are not leaning forward anymore

--- src/main/java/net/asian/civiliansmod/gui/AbstractNPCScreen.java
+++ src/main/java/net/asian/civiliansmod/gui/AbstractNPCScreen.java
@@ -717,12 +717,12 @@ public abstract class AbstractNPCScreen extends AbstractConfigScreen {
         //need to rotate 180°
         if (isPreview) {
             rotation.rotateZ((float) Math.toRadians(180f))
-                    .rotateY((float) Math.toRadians(192.5f))
-                    .rotateX((float) Math.toRadians(-3.5f));
+                    .rotateY((float) Math.toRadians(192.5f));
+                    //.rotateX((float) Math.toRadians(-3.5f));
         } else {
             rotation.rotateZ((float) Math.toRadians(180f))
-                    .rotateY((float) Math.toRadians(165f))
-                    .rotateX((float) Math.toRadians(7f));
+                    .rotateY((float) Math.toRadians(165f));
+                    //.rotateX((float) Math.toRadians(7f));
         }
 
         Quaternionf cameraAngle = new Quaternionf().rotationX((float) Math.toRadians(18f));
